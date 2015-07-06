a3bfbb7ae4a57378205c63c363cc544e6f2a4704
abbottjam <abbottjam@gmail.com>
2015-07-06 16:38:56 +0200

    new 'chrome' layout, added  /data-providers view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,7 @@
 class PagesController < ApplicationController
   def index; end
+
+  def data_providers
+    render :layout => 'chrome'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'pages#index'
+  get 'data-providers' => 'pages#data_providers'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
