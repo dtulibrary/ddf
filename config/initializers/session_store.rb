e60dc3eecc2b719a3c1132f0e600404bfb557eec
abbottjam <abbottjam@gmail.com>
2015-07-30 14:06:53 +0200

    better placement for index actions, search options DO NOT work

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_ddf_session'
+# Rails.application.config.session_store :cookie_store, key: '_ddf_session'
+
+Ddf::Application.config.session_store :active_record_store, { :key => '_ddf_session' }
+# require "#{Rails.root}/lib/blacklight/catalog/search_context.rb"
+
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150623103403) do
+ActiveRecord::Schema.define(version: 20150730090634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,5 +37,15 @@ ActiveRecord::Schema.define(version: 20150623103403) do
   end
 
   add_index "searches", ["user_id"], name: "index_searches_on_user_id", using: :btree
+
+  create_table "sessions", force: :cascade do |t|
+    t.string   "session_id", null: false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", using: :btree
+  add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at", using: :btree
 
 end
