f028d13f46a771258f2f212bec3424fd351b6d29
abbottjam <abbottjam@gmail.com>
2015-09-09 14:57:17 +0200

    added tooltips to chart, doesn't show them, FIXME

--- app/assets/javascripts/visualizations.js
+++ app/assets/javascripts/visualizations.js
@@ -52,23 +52,29 @@ $.ajax({
 });
 
 // function draw(dataset) {
-//   var color  = d3.scale.category20c();
-//   var width  = 450; //360;
-//   var height = 450; //360;
-//   var radius = Math.min(width, height) / 2;
+//   var width          = 720;
+//   var height         = 720;
+//   var radius         = Math.min(width, height) / 2;
+//   var donutWidth     = 75;
+//   var legendRectSize = 18;
+//   var legendSpacing  = 6.5;
+
+//   var color = d3.scale.category20c();
 
 //   var svg = d3.select('#chart')
 //     .append('svg')
 //     .attr('width', width)
 //     .attr('height', height)
 //     .append('g')
-//     .attr('transform', 'translate(' + (width / 2) + ',' + (height / 2) + ')');
+//     .attr('transform', 'translate(' + (width / 2) +
+//       ',' + (height / 2) + ')');
 
 //   var arc = d3.svg.arc()
+//     .innerRadius(radius - donutWidth)
 //     .outerRadius(radius);
 
 //   var pie = d3.layout.pie()
-//     .value(function(d) { return d.count; })
+//     .value(function(d) { return d.hits; })
 //     .sort(null);
 
 //   var path = svg.selectAll('path')
@@ -79,58 +85,42 @@ $.ajax({
 //     .attr('fill', function(d, i) {
 //       return color(d.data.label);
 //     });
+
+//   var legend = svg.selectAll('.legend')
+//     .data(color.domain())
+//     .enter()
+//     .append('g')
+//     .attr('class', 'legend')
+//     .attr('transform', function(d, i) {
+//       var height = legendRectSize + legendSpacing;
+//       var offset =  height * color.domain().length / 2;
+//       var horz = -2 * legendRectSize;
+//       var vert = i * height - offset;
+//       return 'translate(' + horz + ',' + vert + ')';
+//     });
+
+//   legend.append('rect')
+//     .attr('width', legendRectSize)
+//     .attr('height', legendRectSize)
+//     .style('fill', color)
+//     .style('stroke', color);
+
+//   legend.append('text')
+//       .attr('x', legendRectSize + legendSpacing)
+//     //.attr('y', legendRectSize - legendSpacing)
+//     .attr('y', 15)
+//     .text(function(d) { return d; });
 // }
 
-function error() {
-  console.log("error")
-}
-
-// http://localhost:3000/en/catalog.json?utf8=%E2%9C%93&locale=en&q=*:*
-
-// /catalog.json?utf8=%E2%9C%93&locale=en&q=*:*&rows=1
-
-// "facets":[ -> the parent
-// response.facets ->
-// [].each.find_name==format_orig_s ->
-// {}.get_all_items ->
-//  [].each.extract_key_value_pairs
-
-// {
-//   "items":
-//     [
-//       {"value":"dja","hits":367796,"label":"dja"},
-//       {"value":"dba","hits":88849,"label":"dba"},
-//       {"value":"dcp","hits":85135,"label":"dcp"},
-//       {"value":"dca","hits":49126,"label":"dca"},
-//       {"value":"db","hits":37094,"label":"db"},
-//       {"value":"dr","hits":33567,"label":"dr"},
-//       {"value":"dna","hits":20720,"label":"dna"},
-//       {"value":"dco","hits":20005,"label":"dco"},
-//       {"value":"do","hits":17755,"label":"do"},
-//       {"value":"dtp","hits":16809,"label":"dtp"},
-//       {"value":"dw","hits":12919,"label":"dw"},
-//       {"value":"djb","hits":10198,"label":"djb"},
-//       {"value":"dra","hits":9340,"label":"dra"},
-//       {"value":"dbp","hits":8311,"label":"dbp"},
-//       {"value":"djr","hits":5978,"label":"djr"},
-//       {"value":"dln","hits":2586,"label":"dln"},
-//       {"value":"djc","hits":2462,"label":"djc"},
-//       {"value":"dp","hits":1487,"label":"dp"},
-//       {"value":"dtd","hits":748,"label":"dtd"},
-//       {"value":"dd","hits":719,"label":"dd"},
-//       {"value":"dso","hits":173,"label":"dso"}
-//     ],
-//   "name": "format_orig_s",
-//   "label": "translation missing: en.blacklight.search.fields.facet.format_orig_s"
-// }
 
 function draw(dataset) {
+
   var width          = 720;
   var height         = 720;
   var radius         = Math.min(width, height) / 2;
-  var donutWidth     = 75;     // NEW
-  var legendRectSize = 18;     // NEW
-  var legendSpacing  = 6.5;      // NEW
+  var donutWidth     = 75;
+  var legendRectSize = 18;
+  var legendSpacing  = 6.5;
 
   var color = d3.scale.category20c();
 
@@ -139,8 +129,7 @@ function draw(dataset) {
     .attr('width', width)
     .attr('height', height)
     .append('g')
-    .attr('transform', 'translate(' + (width / 2) +
-      ',' + (height / 2) + ')');
+    .attr('transform', 'translate(' + (width / 2) +',' + (height / 2) + ')');
 
   var arc = d3.svg.arc()
     .innerRadius(radius - donutWidth)
@@ -150,6 +139,21 @@ function draw(dataset) {
     .value(function(d) { return d.hits; })
     .sort(null);
 
+
+  var tooltip = d3.select('#chart')            // NEW
+    .append('div')                             // NEW
+    .attr('class', 'tooltip');                 // NEW
+
+  tooltip.append('div')                        // NEW
+    .attr('class', 'label');                   // NEW
+
+  tooltip.append('div')                        // NEW
+    .attr('class', 'hits');                   // NEW
+
+  tooltip.append('div')                        // NEW
+    .attr('class', 'percent');
+
+
   var path = svg.selectAll('path')
     .data(pie(dataset))
     .enter()
@@ -158,6 +162,29 @@ function draw(dataset) {
     .attr('fill', function(d, i) {
       return color(d.data.label);
     });
+
+  path.on('mouseover', function(d) {
+    var total = d3.sum(dataset.map(function(d) {
+      return d.hits;
+    }));
+    var percent = Math.round(1000 * d.data.hits / total) / 10;
+    tooltip.select('.label').html(d.data.label);
+    tooltip.select('.hits').html(d.data.hits);
+    tooltip.select('.percent').html(percent + '%');
+    tooltip.style('display', 'block');
+  });
+
+  path.on('mouseout', function() {                              // NEW
+    tooltip.style('display', 'none');                           // NEW
+  });
+
+
+  // OPTIONAL
+  // path.on('mousemove', function(d) {                            // NEW
+  //   tooltip.style('top', (d3.event.pageY + 10) + 'px')          // NEW
+  //     .style('left', (d3.event.pageX + 10) + 'px');             // NEW
+  // });                                                           // NEW
+
 
   var legend = svg.selectAll('.legend')
     .data(color.domain())
@@ -179,10 +206,8 @@ function draw(dataset) {
     .style('stroke', color);
 
   legend.append('text')
-      .attr('x', legendRectSize + legendSpacing)
-    //.attr('y', legendRectSize - legendSpacing)
+    .attr('x', legendRectSize + legendSpacing)
     .attr('y', 15)
     .text(function(d) { return d; });
+
 }
-
-
