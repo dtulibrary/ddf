6c251f7be3703c6b15c4ef13c31c50f5c9eed354
abbottjam <abbottjam@gmail.com>
2015-09-21 11:18:25 +0200

    testing email delivery with a bang

--- app/controllers/feedback_forms_controller.rb
+++ app/controllers/feedback_forms_controller.rb
@@ -7,13 +7,29 @@ class FeedbackFormsController < ApplicationController
     begin
       @feedback_form = FeedbackForm.new(params[:feedback_form])
       @feedback_form.request = request
-      if @feedback_form.deliver
+      if @feedback_form.deliver!
         flash.now[:notice] = 'Thank you for your message!'
       else
+        # binding.pry
+        flash.now[:notice] = 'Message not delivered'
         render :new
       end
     rescue ScriptError
       flash[:error] = 'Sorry, this message appears to be spam and was not delivered.'
     end
   end
+
+
+  # def create
+  #   @contact_form = ContactForm.new(params[:contact_form])
+  #   @contact_form.request = request
+  #   if @contact_form.deliver
+  #     flash.now[:notice] = 'Thank you for your message!'
+  #   else
+  #     render :new
+  #   end
+  # rescue ScriptError
+  #   flash[:error] = 'Sorry, this message appears to be spam and was not delivered.'
+  # end
+
 end
