6f6eac249991c1439a58784d3148807bec744b73
abbottjam <abbottjam@gmail.com>
2015-09-22 10:22:02 +0200

    dummy email addresses

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,11 +1,15 @@
 class MessageMailer < ApplicationMailer
 
-  default from: "Your Mailer <noreply@yourdomain.com>"
-  default to: "Your Name <vhay@dtu.dk>"
+  default from: "from@example.com"
+  default to:   "to@example.com"
 
   def new_message(message)
     @message = message
 
     mail subject: "Message from #{message.name}"
+  end
+
+  def new_message_stub(opts={})
+    mail subject: "Message from #{opts[:name]}"
   end
 end
