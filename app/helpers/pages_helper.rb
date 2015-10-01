cc86b9d1391dd336f7054b7688b01c74b4b6b13f
abbottjam <abbottjam@gmail.com>
2015-10-01 15:23:25 +0200

    accessible, clickable data visualization by type ready

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,8 +1,13 @@
 module PagesHelper
-   def format_as_pct(value)
+  def format_as_pct(value)
     ["#{value}", "%"].join
   end
-   def parenthesize_as_pct(value)
+
+  def parenthesize_as_pct(value)
     ["(", format_as_pct(value), ")"].join
+  end
+
+  def link_to_facet(facet)
+    link_to facet[:label], catalog_index_path(:f => {:format_orig_s => [facet[:code]]})
   end
 end
