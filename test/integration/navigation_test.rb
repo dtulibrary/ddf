f0f1d9cde1200b65a1a6fdeea03f9ae3b4271213
abbottjam <abbottjam@gmail.com>
2015-10-07 16:54:54 +0200

    some more integration tests

--- test/integration/navigation_test.rb
+++ test/integration/navigation_test.rb
@@ -3,6 +3,13 @@ require "minitest_helper"
 describe "Pages integration" do
   it "shows the home page" do
     visit '/'
-    page.text.must_include "million"
+    page.text.must_include "Publications by Type"
+    first('ul.chartlist').first('li').find('a').click
+    path = 'ul.applied-filters-list li.applied-filter.filter-format_orig_s span.constraint a.remove span.sr-only'
+    page.has_content? 'Remove constraint Type: Journal article'
+    page.has_content? 'foobar'
   end
 end
+
+
+
