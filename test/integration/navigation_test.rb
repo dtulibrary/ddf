ae0ba7629a0dee3e4e1c8d866b2e21ccd86fa3fe
abbottjam <abbottjam@gmail.com>
2015-10-08 10:31:52 +0200

    showing all facets without limit, better hover effects on chartlist

--- test/integration/navigation_test.rb
+++ test/integration/navigation_test.rb
@@ -6,8 +6,10 @@ describe "Pages integration" do
     page.text.must_include "Publications by Type"
     first('ul.chartlist').first('li').find('a').click
     path = 'ul.applied-filters-list li.applied-filter.filter-format_orig_s span.constraint a.remove span.sr-only'
-    page.has_content? 'Remove constraint Type: Journal article'
-    page.has_content? 'foobar'
+    page.has_selector?(path).must_equal true
+    page.has_xpath?(path).must_equal true
+    page.has_content?('Remove constraint Type: Journal article').must_equal true
+    page.has_content?('foobar').must_equal false
   end
 end
 
