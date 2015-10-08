8a517ffe78e4b43292e066f62a979bfdafced68b
abbottjam <abbottjam@gmail.com>
2015-10-08 11:42:39 +0200

    much better selectors for the integration tests

--- test/integration/navigation_test.rb
+++ test/integration/navigation_test.rb
@@ -1,17 +1,30 @@
 require "minitest_helper"
 
 describe "Pages integration" do
-  it "shows the home page" do
-    visit '/'
-    page.text.must_include "Publications by Type"
-    first('ul.chartlist').first('li').find('a').click
-    path = 'ul.applied-filters-list li.applied-filter.filter-format_orig_s span.constraint a.remove span.sr-only'
-    page.has_selector?(path).must_equal true
-    page.has_xpath?(path).must_equal true
-    page.has_content?('Remove constraint Type: Journal article').must_equal true
-    page.has_content?('foobar').must_equal false
+
+  describe "tests for navigating pages by visiting programmatically constructed links" do
+    it "visits a facet page through accessible chart" do
+      visit '/'
+      page.text.must_include "Publications by Type"
+
+      # click a facet
+      find('ul.chartlist.types').find("a[href$='=dja']").click
+
+      path_to_link  = 'ul.applied-filters-list li.applied-filter a.remove'
+      path_to_label = 'ul.applied-filters-list li.applied-filter a.remove span.sr-only'
+
+      page.has_css?(path_to_label).must_equal true
+      page.find(path_to_label).text.must_equal 'Remove constraint Type: Journal article'
+
+      # A bit of redundancy:
+      page.has_content?('Remove constraint Type: Journal article').must_equal true
+      page.has_content?('foobar').must_equal false
+
+      # back to index page
+      find(path_to_link).click
+
+      page.text.must_include "Publications by Type"
+    end
   end
+
 end
-
-
-
