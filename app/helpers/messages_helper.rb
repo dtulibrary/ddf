6da1efcbe2cb8cd4b850c580634c2b7fe7a13b01
Jimmy Petersen <jipe@dtic.dtu.dk>
2015-10-19 13:50:20 +0200

    Fixed feedback mailto link. Refs #2421

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,2 +1,7 @@
 module MessagesHelper
+
+  def feedback_mail_link
+    "mailto:feedbackforskningsdatabase@dtu.dk?subject=#{u "Feedback on forskningsdatabasen.dk (URL: #{request.url})"}"
+  end
+
 end
