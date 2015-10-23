c65d975edecb867f7af3f3fb7751703182537cbc
abbottjam <abbottjam@gmail.com>
2015-10-23 16:39:15 +0200

    added journal subtitle to show view

--- app/helpers/catalog_helper.rb
+++ app/helpers/catalog_helper.rb
@@ -48,47 +48,47 @@ module CatalogHelper
   end
 
   def render_scientific_level_facet(value)
-     t "mxd_type_labels.scientific_level_labels.#{value}"
-  end
+   t "mxd_type_labels.scientific_level_labels.#{value}"
+ end
 
-  def render_source_field args
-    args[:document]['source_ss'].collect {|s| source_label(s)}.join ' ; '
-  end
+ def render_source_field args
+  args[:document]['source_ss'].collect {|s| source_label(s)}.join ' ; '
+end
 
-  def render_format_field_index args
-    t "mxd_type_labels.publication_type_labels.#{args[:document]['format_orig_s']}"
-  end
+def render_format_field_index args
+  t "mxd_type_labels.publication_type_labels.#{args[:document]['format_orig_s']}"
+end
 
-  def render_research_area_field args
-    args[:document]['research_area_ss'].collect {|s| t "research_area_labels.#{s}"}.join ' ; '
-  end
+def render_research_area_field args
+  args[:document]['research_area_ss'].collect {|s| t "research_area_labels.#{s}"}.join ' ; '
+end
 
-  def render_publishing_status(opts)
-    field = opts[:document][opts[:field]]
-    if field.is_a? Array
-      field.map { |s| t "mxd_type_labels.publishing_status_labels.#{s}" }.join ' ; '
-    else
-      t "mxd_type_labels.publishing_status_labels.#{field}"
-    end
+def render_publishing_status(opts)
+  field = opts[:document][opts[:field]]
+  if field.is_a? Array
+    field.map { |s| t "mxd_type_labels.publishing_status_labels.#{s}" }.join ' ; '
+  else
+    t "mxd_type_labels.publishing_status_labels.#{field}"
   end
+end
 
-  def render_language(opts)
-    field = opts[:document][opts[:field]]
-    if field.is_a? Array
-      field.map { |s| t "mxd_type_labels.language_labels.#{s}" }.join ' ; '
-    else
-      t "mxd_type_labels.language_labels.#{field}"
-    end
+def render_language(opts)
+  field = opts[:document][opts[:field]]
+  if field.is_a? Array
+    field.map { |s| t "mxd_type_labels.language_labels.#{s}" }.join ' ; '
+  else
+    t "mxd_type_labels.language_labels.#{field}"
   end
+end
 
-  def render_type(opts)
-    field = opts[:document][opts[:field]]
-    if field.is_a? Array
-      field.map { |s| t "mxd_type_labels.publication_type_labels.#{s}" }.join ' ; '
-    else
-      t "mxd_type_labels.publication_type_labels.#{field}"
-    end
+def render_type(opts)
+  field = opts[:document][opts[:field]]
+  if field.is_a? Array
+    field.map { |s| t "mxd_type_labels.publication_type_labels.#{s}" }.join ' ; '
+  else
+    t "mxd_type_labels.publication_type_labels.#{field}"
   end
+end
 
   # Try toggling the comments on & off, watch what happens to translations
   # of labels above. Also, the method body works in spite of being a duplication
@@ -182,6 +182,7 @@ module CatalogHelper
     document = args[:document]
     field = args[:field]
     [render_journal_title_info(document, format),
+     render_journal_subtitle_info(document, format),
      render_journal_pub_date_info(document, format),
      render_journal_vol_info(document, format),
      render_journal_issue_info(document, format),
@@ -208,6 +209,14 @@ module CatalogHelper
   def render_journal_title_info document, format
     if document['journal_title_ts']
       "#{document['journal_title_ts'].first}"
+    else
+      ''
+    end
+  end
+
+  def render_journal_subtitle_info document, format
+    if document['journal_subtitle_ts']
+      ": #{document['journal_subtitle_ts'].first}"
     else
       ''
     end
