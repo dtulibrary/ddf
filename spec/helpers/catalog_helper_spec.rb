f167ed201a3d7d206b3ad3a469c8002ca3450b68
Ronan McHugh <mchugh.r@gmail.com>
2015-12-08 09:17:52 +0100

    fix translation

--- spec/helpers/catalog_helper_spec.rb
+++ spec/helpers/catalog_helper_spec.rb
@@ -4,13 +4,13 @@ describe CatalogHelper do
     context 'using an inherited value' do
       let(:source) { :orbit }
       it 'uses the default values from the sources' do
-        expect(subject).to eql 'Technical University of Denmark'
+        expect(subject).to eql I18n.t('mxd_type_labels.source_labels.orbit')
       end
     end
     context 'using an overridden value' do
       let (:source) { :rdb_ark }
       it 'overwrites with an updated value for rdb_ark' do
-        expect(subject).to eql 'Research in Architecture, Design and Conservation'
+        expect(subject).to eql I18n.t('mxd_type_labels.facet_source_labels.rdb_ark')
       end
     end
   end
