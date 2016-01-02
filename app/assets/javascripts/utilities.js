18dbf4377833ec7e4023317f32567c942fcf5816
abbottjam <abbottjam@gmail.com>
2016-01-02 10:06:24 +0100

    better styling of logos and cards, improved toggle function

--- app/assets/javascripts/utilities.js
+++ app/assets/javascripts/utilities.js
@@ -18,22 +18,20 @@ $(document).ready(function() {
 // Fetch the target list item with that class
 // slideToggle that item
 $(document).ready(function() {
-
   $("ul.data-provider-logos-list li a").click(function(event) {
     event.preventDefault();
+
     // $(this).parent().toggleClass("logo-clicked"); // not working very well
+    console.log( $(this).parent() );
 
     // remove visibility from previously chosen item (for when list.length > 1)
     previous = $("ul.data-provider-cards-list li[style^='display: list-item']")
-    // console.log(previous);
-    previous.slideToggle("fast");
-
     // klass = $("ul.data-provider-logos-list li").attr("class")
     klass = $(this).parent().attr("class") // alternative to above
     item = $("ul.data-provider-cards-list li[class^=" +klass+ "]")
 
-    item.slideToggle("medium");
-    // alert(item);
+    if (!previous.is(item)) { previous.slideToggle("fast"); }
+    item.slideToggle("slow");
     //console.log(item);
   });
 
