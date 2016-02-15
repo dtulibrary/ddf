e13874e64a2dcae9579c5724dbeb4cceb37fa878
abbottjam <abbottjam@gmail.com>
2016-02-15 14:45:34 +0100

    linked OA development charts to overview page

--- app/helpers/open_access_indicator_helper.rb
+++ app/helpers/open_access_indicator_helper.rb
@@ -1,33 +1,13 @@
 module OpenAccessIndicatorHelper
-  def format_as_pct(value)
-    ["#{value}", "%"].join
+  def render_oa_unit_heading(unit)
+    if unit.eql? "national"
+      "<h3 class='oa-label oa-label--total'>#{t("ddf.open_access.labels.#{unit}")}</h3>".html_safe
+    else
+      "<h3 class='oa-label'>#{t("ddf.open_access.labels.#{unit}")}</h3>".html_safe
+    end
   end
 
-  def parenthesize_as_pct(value)
-    ["(", format_as_pct(value), ")"].join
-  end
-
-  # TODO for OA years
-  # Creates a link to a facet, like:
-  # http://localhost:3000/en/catalog?f[format_orig_s][]=dja
-  # Pattern:
-  # link_to 'label', catalog_index_path(:f => {:format_orig_s => ['dja']})
-  # def render_link_to(facet)
-  #   link_to facet[:label], catalog_index_path(:f => {facet[:name] => [facet[:code]]})
-  # end
-
-  # def render_count_for(facet)
-  #   num = number_with_delimiter(facet[:count])
-  #   "<span class='count'>#{num}</span>".html_safe
-  # end
-
-  # def render_percentage_for(facet)
-  #   pct = format_as_pct(facet[:pct])
-  #   parenthesized = parenthesize_as_pct(facet[:pct])
-  #   "<span class='pct' style='width: #{pct}'>#{parenthesized}</span>".html_safe
-  # end
-
-  def render_year_for(year)
+  def render_label_for(year)
     "<span class='label'>#{year}</span>".html_safe
   end
 
@@ -37,6 +17,9 @@ module OpenAccessIndicatorHelper
     "<span class='count' style='height: #{pct}%'>#{rounded}%</span>".html_safe
   end
 
+  def render_overview_link_for(year)
+    [open_access_overview_path, "?year=#{year}"].join
+  end
 end
 
 
