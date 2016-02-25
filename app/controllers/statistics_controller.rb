55e0164afe38c9080b4186a53e60f920f94e010f
abbottjam <abbottjam@gmail.com>
2016-02-25 15:34:14 +0100

    duplicated former /index charts into /statistics

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,3 +1,9 @@
 class StatisticsController < ApplicationController
-  def index; end
+  include StatService
+  layout "statistics"
+
+  def index
+    @types = publications_by_facet('format_orig_s', limit: 50)
+    @institutions = publications_by_facet('source_ss', limit: 50)
+  end
 end
