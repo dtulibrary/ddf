598336ca549994b7f4bd26096d6d8efcd62a0ff8
abbottjam <abbottjam@gmail.com>
2016-02-26 12:55:50 +0100

    changed the OA test profile configuration from test to production

--- config/application.rb
+++ config/application.rb
@@ -47,7 +47,7 @@ module Ddf
 
     config.x.open_access.url = 'http://oa-indicator.cvt.dk/oa-indicator/ws/%{resource}.%{format}/%{year}/%{profile}'
     config.x.open_access.status_api = 'http://oa-indicator.cvt.dk/oa-indicator/ws/status'
-    config.x.open_access.api_profile = 'test'
+    config.x.open_access.api_profile = 'prod'
     config.x.open_access.last_available_year = 2014
     config.x.open_access.total_years = (2013..2021)
     config.x.open_access.report_url = 'http://oa-indicator.cvt.dk/oa-indicator-runs/%{year}.%{profile}/spreadsheets/%{lang}/%{filename}'
