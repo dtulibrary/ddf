bad3ba42f21891ac25a1dcc0b0c68ee46cc2ee39
Ronan McHugh <mchugh.r@gmail.com>
2016-03-04 10:56:44 +0100

    move autocomplete to common gem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,12 +15,11 @@
 //
 // Required by Blacklight:
 //= require blacklight/blacklight
-//
+//= require dtu/dtu
 //= require_tree .
+
 
 
 // For blacklight_range_limit built-in JS, if you don't want it you don't need
 // this:
 //= require 'blacklight_range_limit'
-
-//= require jquery-ui
