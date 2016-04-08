e14d708dd1107378f632fe6217ba3554065144d3
Ronan McHugh <mchugh.r@gmail.com>
2016-04-08 11:46:35 +0200

    clone via https for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.4.0'
 
 set :application, 'ddf'
-set :repo_url, 'git@github.com:dtulibrary/ddf.git'
+set :repo_url, 'https://github.com/dtulibrary/ddf.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
