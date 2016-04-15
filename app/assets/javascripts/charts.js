f7eb95e31333df9ed56a7e2f86033a731da87868
abbottjam <abbottjam@gmail.com>
2016-04-15 14:35:26 +0200

    Review Type Chart.js doughnut now works as navigation; TODO: locales, label translations

--- app/assets/javascripts/charts.js
+++ app/assets/javascripts/charts.js
@@ -14,8 +14,39 @@ drawPlot = function(container) {
   return new Chart(ctx).Bar(json);
 }
 
+
+var translations = {}
+translations['Peer Review'] = 'peer_review';
+translations['Undetermined'] = 'undetermined';
+translations['Ubestemt'] = 'undetermined';
+
 $(document).ready(function() {
   var reviewChart = drawSegments("review-type");
+
+  // TODO
+  $("#review-type").click(
+    function(evt) {
+      var activePoints = reviewChart.getSegmentsAtEvent(evt);
+      var segment = translations[activePoints[0].label];
+      var canvas = document.getElementById("review-type");
+      var facet_frag = canvas.dataset.facet;
+
+      console.log(segment);
+
+      // var url = "http://example.com/?label=" + activePoints[0].label + "&value=" + activePoints[0].value;
+
+      var url = "/catalog?f[" +facet_frag+ "][]=" +segment+ "&q=*:*";
+
+      window.location = url;
+    }
+  );
+  // TODO
+
+  // Mine:
+  // http://localhost:3000/catalog?f[review_status_s][]=undertermined&q=*:*
+  // Facet:
+  // http://localhost:3000/en/catalog?f[review_status_s][]=undetermined&q=*%3A*
+
   var sciLevelChart = drawSegments("scientific-level");
   var resAreaChart = drawSegments("research-area");
   var pubStatusChart = drawSegments("publication-status");
@@ -23,21 +54,14 @@ $(document).ready(function() {
   var pubYearChart = drawPlot("publication-year");
 });
 
-// Vary on: 1) data range; 2) interval step, i.e. 10, 50 etc years
-// var lineChartData = {
-//   // hashify('pub_date_tsort').sort.map { |pair| pair.first }
-//   labels: ["1800", "1948", "1949", "1950", "1951", "1952", "1953", "1954", "1955", "1956", "1957", "1958", "1959", "1960", "1961", "1962", "1963", "1964", "1965", "1966", "1967", "1968", "1969", "1970", "1971", "1972", "1973", "1974", "1975", "1976", "1977", "1978", "1979", "1980", "1981", "1982", "1983", "1984", "1985", "1986", "1987", "1988", "1989", "1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019"],
-//   datasets: [
-//     {
-//       label: "My First dataset",
-//       fillColor: "rgba(220,220,220,0.2)",
-//       strokeColor: "rgba(220,220,220,1)",
-//       pointColor: "rgba(220,220,220,1)",
-//       pointStrokeColor: "#fff",
-//       pointHighlightFill: "#fff",
-//       pointHighlightStroke: "rgba(220,220,220,1)",
-//       // hashify('pub_date_tsort').sort.map { |pair| pair.last }
-//       data: [10, 2, 6, 22, 18, 16, 16, 25, 28, 51, 52, 78, 82, 66, 81, 96, 137, 169, 217, 202, 251, 277, 300, 315, 407, 433, 538, 563, 597, 664, 775, 882, 1014, 1137, 1235, 1402, 1556, 1673, 1847, 2067, 2399, 2791, 3364, 4440, 7223, 8974, 9546, 12126, 12546, 14905, 18612, 19742, 25121, 25283, 29132, 31292, 32332, 34212, 35952, 39162, 40317, 45122, 46391, 47744, 49792, 50317, 50363, 51349, 50738, 10719, 80, 15, 1]
+//
+// http://stackoverflow.com/questions/26257268/click-events-on-pie-charts-in-chart-js
+//
+// $("#myChart").click(
+//     function(evt){
+//         var activePoints = myNewChart.getSegmentsAtEvent(evt);
+//         var url = "http://example.com/?label=" + activePoints[0].label + "&value=" + activePoints[0].value;
+//         alert(url);
 //     }
-//   ]
-// };
+// );
+//
--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -3,18 +3,18 @@ class StatisticsController < ApplicationController
   layout "statistics"
 
   def index
-    # No JS charts
-    @types = Charts::CSSBars.new('format_orig_s').values
-    @institutions = Charts::CSSBars.new('source_ss').values
-
     # Chart JS doughnut / pie
-    @review = Charts::Segments.new('review_status_s').values.to_json
-    @sci_level = Charts::Segments.new('scientific_level_s').values.to_json
-    @res_area = Charts::Segments.new('research_area_ss').values.to_json
-    @pub_status = Charts::Segments.new('access_condition_s').values.to_json
+    @by_review_type = Charts::Segments.new('review_status_s').values.to_json
+    @by_sci_level = Charts::Segments.new('scientific_level_s').values.to_json
+    @by_research_area = Charts::Segments.new('research_area_ss').values.to_json
+    @by_publication_status = Charts::Segments.new('access_condition_s').values.to_json
 
     # Chart JS bar / line
     facets = ['pub_date_tsort', 'submission_year_tis']
-    @pub_year = Charts::Plot.new(facets).from(1950).interval(5).values.to_json
+    @by_year = Charts::Plot.new(facets).from(1960).interval(5).values.to_json
+
+    # No JS charts
+    @by_type = Charts::CSSBars.new('format_orig_s').values
+    @by_institution = Charts::CSSBars.new('source_ss').values
   end
 end
