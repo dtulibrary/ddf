bf31ee76330401767ae0a40577d4d3024cf30aa6
abbottjam <abbottjam@gmail.com>
2016-04-18 15:56:09 +0200

    added clickability for sci level, generic clickable function needs more work

--- app/assets/javascripts/charts.js
+++ app/assets/javascripts/charts.js
@@ -7,22 +7,22 @@ drawSegments = function(container) {
   return new Chart(ctx).Doughnut(json);
 }
 
-makeSegmentsClickable = function(chart, container) {
-  var DOMElement = "#"+container;
-  $("#review-type").click(
-    function(evt) {
+// makeSegmentsClickable = function(chart, container) {
+//   var DOMElement = "#"+container;
+//   $("#review-type").click(
+//     function(evt) {
 
-      var locale = window.location.pathname.split('/')[1];
-      var canvas = document.getElementById(container);
-      var facet_frag = canvas.dataset.facet;
-      var activePoints = chart.getSegmentsAtEvent(evt);
-      var segment = translations[activePoints[0].label];
+//       var locale = window.location.pathname.split('/')[1];
+//       var canvas = document.getElementById(container);
+//       var facet_frag = canvas.dataset.facet;
+//       var activePoints = chart.getSegmentsAtEvent(evt);
+//       var segment = translations[activePoints[0].label];
 
-      var url = "/" +locale+ "/catalog?f[" +facet_frag+ "][]=" +segment+ "&q=*:*";
-      window.location = url;
-    }
-  );
-}
+//       var url = "/" +locale+ "/catalog?f[" +facet_frag+ "][]=" +segment+ "&q=*:*";
+//       window.location = url;
+//     }
+//   );
+// }
 
 drawPlot = function(container) {
   ctx = document.getElementById(container).getContext("2d");
@@ -36,32 +36,52 @@ translations['Peer Review']  = 'peer_review';
 translations['Undetermined'] = 'undetermined';
 translations['Ubestemt']     = 'undetermined';
 
+translations['Scientific']  = 'scientific';
+translations['Popular']     = 'popular';
+translations['Educational'] = 'educational';
+
+translations['Videnskabelig']        = 'scientific';
+translations['Populærvidenskabelig'] = 'popular';
+translations['Undervisningsrettet']  = 'educational';
+
 $(document).ready(function() {
   var reviewChart = drawSegments("review-type");
-  makeSegmentsClickable(reviewChart, "review-type");
-
-  // console.log(reviewChart);
+  // makeSegmentsClickable(reviewChart, "review-type");
 
   // TODO
-  // $("#review-type").click(
-  //   function(evt) {
-  //     var activePoints = reviewChart.getSegmentsAtEvent(evt);
+  $("#review-type").click(
+    function(evt) {
 
-  //     // console.log(activePoints);
-  //     // alert(activePoints);
+      var locale = window.location.pathname.split('/')[1];
+      var canvas = document.getElementById("review-type");
+      var facet_frag = canvas.dataset.facet;
+      var activePoints = reviewChart.getSegmentsAtEvent(evt);
+      var segment = translations[activePoints[0].label];
 
-  //     var locale = window.location.pathname.split('/')[1];
-  //     var canvas = document.getElementById("review-type");
-  //     var facet_frag = canvas.dataset.facet;
-  //     var segment = translations[activePoints[0].label];
-
-  //     var url = "/" +locale+ "/catalog?f[" +facet_frag+ "][]=" +segment+ "&q=*:*";
-  //     window.location = url;
-  //   }
-  // );
+      var url = "/" +locale+ "/catalog?f[" +facet_frag+ "][]=" +segment+ "&q=*:*";
+      window.location = url;
+    }
+  );
   // TODO
 
   var sciLevelChart = drawSegments("scientific-level");
+  // makeSegmentsClickable(sciLevelChart, "scientific-level");
+
+    $("#scientific-level").click(
+    function(evt) {
+
+      var locale = window.location.pathname.split('/')[1];
+      var canvas = document.getElementById("scientific-level");
+      var facet_frag = canvas.dataset.facet;
+      var activePoints = sciLevelChart.getSegmentsAtEvent(evt);
+      var segment = translations[activePoints[0].label];
+
+      var url = "/" +locale+ "/catalog?f[" +facet_frag+ "][]=" +segment+ "&q=*:*";
+      window.location = url;
+    }
+  );
+
+
   var resAreaChart = drawSegments("research-area");
   var pubStatusChart = drawSegments("publication-status");
 
