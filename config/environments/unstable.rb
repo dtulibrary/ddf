961254985b57882d4552ce1887933bd8c5328b85
Ronan McHugh <mchugh.r@gmail.com>
2016-05-23 13:46:19 +0200

    shouldn't pass object to block

--- config/environments/unstable.rb
+++ config/environments/unstable.rb
@@ -1,6 +1,6 @@
 load File.dirname(__FILE__) + '/production.rb'
 
-Rails.application.configure do |config|
+Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'spotlight.ddf.dtic.dk' }
 end
 
