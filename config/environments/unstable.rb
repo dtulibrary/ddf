3be04ae7990d577e9d672b31cd7a99341340f4f6
Ronan McHugh <mchugh.r@gmail.com>
2016-08-01 14:32:31 +0200

    add unstable monitoring for test

--- config/environments/unstable.rb
+++ config/environments/unstable.rb
@@ -3,3 +3,6 @@ load File.dirname(__FILE__) + '/production.rb'
 if File.exists? File.dirname(__FILE__) + '/../application.local.rb'
   require File.dirname(__FILE__) + '/../application.local.rb'
 end
+Rails.application.configure do
+  config.x.monitoring_id = 'ddf_unstable'
+end
